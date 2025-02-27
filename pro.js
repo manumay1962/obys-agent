55a697f5c88bc30b71bae51401e9b8783594f56b
ManumayRajMishra <manumayrm@gmail.com>
2025-02-27 09:40:53 +0530

    pages made

---
+++ pro.js
@@ -0,0 +1,76 @@
+function loadinganimation(){
+    var tl=gsap.timeline();
+
+tl.from(".line h1",{
+    y:150,
+    stagger:0.25,
+    duration:0.4,
+    delay:0.5,
+});
+
+tl.from("#line1-part1, .line h2",{
+    opacity:0,
+    onStart:function(){
+        var h5timer=document.querySelector("#line1-part1 h5")
+        let grow=0;
+
+    setInterval(function(){
+    if(grow<100){
+        h5timer.innerHTML=grow++
+    }else{
+        h5timer.innerHTML=grow
+    }
+},33 );
+    }
+
+});
+
+tl.to(".line h2",{
+    animationName:"anime",
+    opacity:1, 
+})
+
+
+tl.to("#loader",{
+    opacity:0,
+    delay:3.2,
+    duration:0.4
+   
+})
+tl.from("#nav",{
+  opacity:0,
+})
+
+ 
+tl.from("#page1",{
+    delay:0.2,
+    y:1600,
+    opacity:0,
+    duration:0.6,
+    ease:Power4,
+})
+tl.to("#loader",{
+    display:"none"
+    
+})
+tl.from("#hero h1,#hero h2 ",{
+  y:120,
+    stagger:0.2,
+})
+
+}
+loadinganimation();
+
+  function cursorAnimation(){
+
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+           left:dets.x,
+           top:dets.y,
+        })
+        })
+        Shery.makeMagnet("#nav-part2 h4", {
+            
+         });
+  }
+   cursorAnimation(); 
